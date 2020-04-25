fa0c3043a5c5ebab3d3171a95e1092958f290191
alu0101100654 <alu0101100654@ull.edu.es>
2020-04-25 10:43:14 +0100

    Funcionalidad ParabolMovement

--- src/parabol-movement.js
+++ src/parabol-movement.js
@@ -30,28 +30,56 @@
 const GRAVITY_VALUE = 9.8;
 
 class ParabolMovement {
-  constructor(angle, initialSpeed, initialHeight = 0) {
+  constructor(angle, initialSpeed, time, initialHeight = 0) {
     this._angle = angle;
-    this._initialSpedd = initialSpeed;
+    this._initialSpeed = initialSpeed;
     this._initialHeight = initialHeight;
+    this._time = time;
+    this.calculate();
   }
   get angle() {
     return this._angle;
   }
   get initialSpeed() {
-    return this._initialSpedd;
+    return this._initialSpeed;
   }
   get initialHeight() {
     return this._initialHeight;
   }
+  get xSpeed() {
+    return this._xSpeed;
+  }
+  get ySpeed() {
+    return this._ySpeed;
+  }
+  get xPosition() {
+    return this._xPosition;
+  }
+  get yPosition() {
+    return this._yPosition;
+  }
   set angle(newAngle) {
     this._angle = newAngle;
+    this.calculate();
   }
   set initialSpeed(newSpeed) {
     this._initialSpedd = newSpeed;
+    this.calculate();
   }
   set initialHeight(newHeight) {
     this._initialHeight = newHeight;
+    this.calculate();
+  }
+  calculate() {
+    this._xSpeed = this._initialSpeed * Math.cos(this._angle);
+    this._ySpeed = this._initialSpeed * Math.sin(this._angle) -
+      GRAVITY_VALUE * this._time;
+    this._xPosition = this._xSpeed * this._time;
+    this._yPosition = this._initialHeight + this._ySpeed * this._time -
+      0.5 * GRAVITY_VALUE * Math.pow(this._time, 2);
+    if (this._yPosition < 0) {
+      this._yPosition = 0;
+    }
   }
 };
 
