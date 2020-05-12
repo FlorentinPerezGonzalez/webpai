6aa40c7498204c883d65d1e136fcc68e83a4ba5d
alu0101100654 <alu0101100654@ull.edu.es>
2020-05-12 12:29:46 +0100

    version1

---
+++ src/ejercicio/index.js
@@ -0,0 +1,25 @@
+/**
+ * @version 1.0.0
+ * @author Florentín Pérez Glez. <alu0101100654@ull.edu.es>
+ * @file Módulo principal que gestiona la ejecución de un programa destinado
+ * para navegadores que permite mostrar una bola sobre un canvas y desplazarla.
+ * @copyright Florentín Pérez Glez 2020
+ * @since 09.05.2020
+ * @desc
+ * Universidad: Universidad de La Laguna.
+ *
+ * Asignatura: Programación de Aplicaciones Interactivas.
+ *
+ * Curso: 3º
+ *
+ * Práctica 12. Juego de la vida. Programación Gráfica, Orientada a Objetos y
+ * dirigida por eventos en JS.
+ *
+ * Contenido detallado: Contiene la implementación de la clase LifeGameDisplay, que permite
+ * la representación gráfica del "Juego de la vida".
+ *
+ * Referencias:
+ *    - Enunciado de la práctica:
+ *      https://github.com/fsande/PAI-P12-Life/blob/master/2019-2020_p12_Life.md
+ *
+ */
